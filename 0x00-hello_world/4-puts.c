add1e1ee7e77deb3bdc71aa42e43444f12dd5d25
Ezekiel <ezekieljeremiah92@gmail.comgit config --global user.email ezekieljeremiah92@gmail.com>
2022-09-09 02:36:40 -0700

    my script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /**
  * main-Serves a the entry point for the program 
- *
- * Return: By defualt returns zero if no errors otherwise returns non-zero value.
- */
-int main(void) 
-{
-	puts("\" Programming is like buildinng a mulitilingual puzzle");
-         return(0);
+  * Return: By defualt returns zero if no errors otherwise returns non-zero valu */
+	int main(void) 
+{	
+	puts("\"Programming is like buildinga multilingual
+puzzle");
+	return (0);
 }
