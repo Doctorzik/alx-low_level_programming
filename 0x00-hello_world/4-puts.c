af573beb80c46be209788d4ce83dc795e7827438
Ezekiel <ezekieljeremiah92@gmail.comgit config --global user.email ezekieljeremiah92@gmail.com>
2022-09-09 01:56:20 -0700

    my programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-Serves a the entry point for the program 
+ *
+ * Return: By defualt returns zero if no errors otherwise returns non-zero value.
+ */
+int main(void) 
+{
+	puts("\" Programming is like buildinng a mulitilingual puzzle");
+         return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -is the entry point for the program
+ * Return:zero if no error, Non-zerovalue if errors.
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n",sizeof(char));
+	printf("Sizeof an int: %ld byte(s)\n",sizeof(int));
+	printf("Size of a longint: %ld byte(s)\n",sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float);
+	
+	return(0);
+}
